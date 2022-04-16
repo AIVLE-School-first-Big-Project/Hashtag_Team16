f32a325c8b778bb2234acc72d92b1fd62f7bdf8a
sonhw <ths1611@naver.com>
2022-04-16 18:31:55 +0900

    title app 추가 및 bs 적용

--- fisrtapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FisrtappConfig(AppConfig):
-    name = 'fisrtapp'
--- main/views.py
+++ main/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+def index1(request):
+    return HttpResponse('<u>Hello</u>')
 # Create your views here.

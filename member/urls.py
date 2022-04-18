b1360fd38a51e379dbf53edd7436066e0e9e80b6
hyeonnis <59681564+seohyeon24@users.noreply.github.com>
2022-04-18 16:19:01 +0900

    서현_signup에서 custom뺀다

--- member/urls.py
+++ member/urls.py
@@ -4,5 +4,5 @@ from member.views import *
 
 urlpatterns = [
     path('login/custom/', login_custom, name='login_custom'),
-    path('signup/custom/', signup_custom, name='signup_custom'),
+    path('signup', signup_custom, name='signup_custom'),
 ]

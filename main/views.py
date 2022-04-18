6fa79683bd7a5b142a768b57f026cfebf2da393f
Jimi-Neotronu <wonside99@naver.com>
2022-04-18 11:11:32 +0900

    main

--- main/views.py
+++ main/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 def index1(request):
     return HttpResponse('<u>Hello</u>')
 # Create your views here.
+
+def index(request):
+    return render(request, 'main/index.html')

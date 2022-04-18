889e229743be02a612e7a5b791f578e9d4e95caa
serimkoh <66110901+serimkoh@users.noreply.github.com>
2022-04-18 16:36:50 +0900

    member뷰

--- member/views.py
+++ member/views.py
@@ -18,28 +18,11 @@ def login_custom(request):
             return HttpResponse('로그인 실패')
         else:
             request.session['user_id'] = user.user_id
-            #request.session['user_name'] = user.name
+            request.session['user_name'] = user.name
         # 회원정보 조회 실패시 예외 발생
         
         return render(request, 'member/login_custom.html')
+        #return redirect('member:login')
         #return redirect('index')
     else:
         return render(request, 'member/login_custom.html')
-
-        
-from .models import USER
-from django.utils import timezone
-from django.http import HttpResponse
-def signup_custom(request):
-    if request.method == 'POST':
-        user_id = request.POST.get('user_id')
-        user_pw = request.POST.get('user_pw')
-        user_name = request.POST.get('user_name')
-        m = Member(
-        user_id=user_id, user_pw=user_pw, user_name=user_name)
-        m.date_joined = timezone.now()
-        m.save()
-        return HttpResponse(
-            '가입 완료<br>%s %s %s' % (user_id, user_pw, user_name))
-    else:
-         return render(request, 'member/signup_custom.html')

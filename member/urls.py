4ab50835b7f24ef88522b1b7a7841bc25cc939f4
hyeonnis <59681564+seohyeon24@users.noreply.github.com>
2022-04-18 15:50:57 +0900

    서현_회원가입기능추가

--- member/urls.py
+++ member/urls.py
@@ -4,4 +4,5 @@ from member.views import *
 
 urlpatterns = [
     path('login/custom/', login_custom, name='login_custom'),
+    path('signup/custom/', signup_custom, name='signup_custom'),
 ]
--- member/views.py
+++ member/views.py
@@ -16,3 +16,21 @@ def login_custom(request):
         return redirect('index')
     else:
         return render(request, 'member/login_custom.html')
+
+        
+from .models import USER
+from django.utils import timezone
+from django.http import HttpResponse
+def signup_custom(request):
+    if request.method == 'POST':
+        user_id = request.POST.get('user_id')
+        user_pw = request.POST.get('user_pw')
+        user_name = request.POST.get('user_name')
+        m = Member(
+        user_id=user_id, user_pw=user_pw, user_name=user_name)
+        m.date_joined = timezone.now()
+        m.save()
+        return HttpResponse(
+            '가입 완료<br>%s %s %s' % (user_id, user_pw, user_name))
+    else:
+         return render(request, 'member/signup_custom.html')

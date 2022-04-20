cc0514ac4d9c3c235945d43e39feceeec4f160a4
sonhw <ths1611@naver.com>
2022-04-20 13:00:55 +0900

    오후 1:00

--- main/views.py
+++ main/views.py
@@ -1,9 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index1(request):
-    return HttpResponse('<u>Hello</u>')
-# Create your views here.
-
 def index(request):
     return render(request, 'main/index.html')

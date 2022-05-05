48307b8ce0699d12a4d6ade6054d2a8b8de02ce7
sonhw <ths1611@naver.com>
2022-05-05 22:07:58 +0900

    해시태그 출력, 사용량분석 : 멀티프로세싱 적용

--- main/views.py
+++ main/views.py
@@ -12,6 +12,8 @@ import hashlib # image 저장할때 이름을 암호화하기 위함
 import json
 import requests
 from multiprocessing import Pool
+import mp_module
+
 ###############################################################
 
 def index(request):
@@ -73,24 +75,13 @@ class hashtag(View):
         # 해쉬태그 생성 API
         files = open(url, 'rb')
         upload = {'file': files}
-        res = requests.post(' http://192.168.137.1:5001/hashtags20/', files = upload)
+        res = requests.post('http://118.91.69.43:5001/hashtags20/', files = upload)
         hashtags_json = json.loads(res.content)
         files.close()
         os.remove(url)
         data = {'hashtag' : hashtags_json['hashtags']};
+        data['best_hash'] = mp_module.mult_process(hashtags_json['hashtags'][:5])
 
-
-
-        from concurrent.futures import ProcessPoolExecutor
-        with ProcessPoolExecutor(max_workers=4) as pool:
-            print(pool.map(self.hashtag_cnt_crawling, hashtags_json['hashtags'][:5]))
-
-
-        print(hashtags_json['hashtags'][:5])
-        pool = Pool(4)
-        squares = pool.map(hashtag_cnt_crawling, hashtags_json['hashtags'][:5]) 
-        print(squares)
-        
         return JsonResponse(data);
 
 class image_upload_save(View):
@@ -132,16 +123,19 @@ def image_func(tmp_file, image_name):
 
 
 ##################해시태그 게시글 수 크롤링######################
-def hashtag_cnt_crawling(self, target):
+def hashtag_cnt_crawling(target):
     url = 'https://www.instagram.com/explore/tags/'+ target +'/?__a=1&__d=dis'
+    print(url)
     try:
+        print('try')
         request_headers = {
-            'cookie': 'ig_did=9218D585-1D45-43F2-8CD8-551A16853932; ig_nrcb=1; mid=YmwBYAALAAH-JdcTDWyYG3JrAfjD; fbm_124024574287414=base_domain=.instagram.com; datr=BF5tYmZtnVsJotuwxMLxEXCS; shbid="11862\0541994601200\0541683075730:01f73c42489b8d6f5ee1f821a6aad3eeb95f5cf1b997832aa7aefcb86c45ef5885a25d9f"; shbts="1651539730\0541994601200\0541683075730:01f72f006f5f41b9af5b62a6d9f36cdb6b0c4cc89564cd99752f25f70597f1ed97fffce7"; csrftoken=lF08QydrVyuCWNssIa4HCJ5gRpSGsiF6; ds_user_id=1994601200; sessionid=1994601200%3AbHVDMgen5K46Ph%3A29; fbsr_124024574287414=o1UVaSf7ZKz-klW0FCWZwzhJJUvU2OQVL86LZy8bMAQ.eyJ1c2VyX2lkIjoiMTAwMDAzMjU2MDc3NDUzIiwiY29kZSI6IkFRQUFKYVlsU1dFb21VQnF1ZXdKdkVsOVY5RElxajV2aTE3SFFKNXJ2cnhhVGhmQzlMNjJFRFlGd1FYampUYjg0UUt2VDFiN0ktcGRmU1J1V0lPTFhqOU9KVUhqR2NFeVAyMEMtWVhPNWU1VFh3NF9WZWE5U3B5Q0dPLVdKYkxUZDVQdGRNeWNqWFJ6WFZOc0dfVGZsT09BOXpTajk4emZDOVpCQkxobnU1cEFiMkFfRjA5T0JzQUVhbk1aMG9qcDdWeWY2eHRPdVdnVS1vbGt6TjV2OFJ0eExXZ19mYzBYdUV1NUJhWmdaWTMtaTdNWlBCRUJmUEpYZE1maGx4YkRoWkEwMDhXMnZQS2ZBMHB6UlVrUHBZZ2s0M1hsbTRvcmdLZVFUOWFzcUlDUDFtREV1OF9NazJNNEpaVWNkYXZ4UkQ3YmU5bDFNVG4zUHJ0SXl4aHZHSjdXIiwib2F1dGhfdG9rZW4iOiJFQUFCd3pMaXhuallCQUtMMkJ5N2I3clhkTmVUbkJIMGNaQ3dYWkNneTZwWHA4SDRwdDN2SE1RWkF1TEYyTGJQeEppUUhVOHhBQVI0aVltVlpCYTM2ZEpoZVFaQWZsNnVJbEdnZmJFY3h2cWREOGZOSzgwTnJaQVJHd1JaQnRiMjlaQ01YZ3RQSnFIRmpmeFpDcTNHNkRXeHd3eFBZeHc1c1k5Vnp3VVNMRTZNUWZqRmpVQ0hvR1IyVk9YeDduUlpCUmRrbVFaRCIsImFsZ29yaXRobSI6IkhNQUMtU0hBMjU2IiwiaXNzdWVkX2F0IjoxNjUxNzQwMDAyfQ; rur="VLL\0541994601200\0541683276024:01f7a867559c84d4d4716bf608c3fc46112b7570755b15ce466f54316efd0bdd77638b11"'
-            } 
+            'cookie' : 'ig_did=9218D585-1D45-43F2-8CD8-551A16853932; ig_nrcb=1; mid=YmwBYAALAAH-JdcTDWyYG3JrAfjD; fbm_124024574287414=base_domain=.instagram.com; datr=BF5tYmZtnVsJotuwxMLxEXCS; shbid="11862\0541994601200\0541683075730:01f73c42489b8d6f5ee1f821a6aad3eeb95f5cf1b997832aa7aefcb86c45ef5885a25d9f"; shbts="1651539730\0541994601200\0541683075730:01f72f006f5f41b9af5b62a6d9f36cdb6b0c4cc89564cd99752f25f70597f1ed97fffce7"; csrftoken=lF08QydrVyuCWNssIa4HCJ5gRpSGsiF6; ds_user_id=1994601200; sessionid=1994601200%3AbHVDMgen5K46Ph%3A29; fbsr_124024574287414=zM2yABgZqpJGemiq8pJzm7b9oOH7J-CQIi0mucWPaIE.eyJ1c2VyX2lkIjoiMTAwMDAzMjU2MDc3NDUzIiwiY29kZSI6IkFRRFNlTkhaXzh2MEZUN01CQ040OENqTVdBc1ZpZHBCcm1mMWlDY0JWa0l0NmRkaWpES2dvWGRSMEkyZmtLT200dTlKclhCOExGUDUtSGRvS1pZb2VoMmo5eUZScWlBWjdRTGZhd1NMWFJCRXdacjUzeWFGWjdzTFdsRGIwajZwc1JsTTB3TGYtaXdNaXhGTmMtVHlseEJJZFBUN2FDZUhyMkIyWGcxMzRhR1hOZ2NoS1pmWXR2ZGU2bVZadEZ2MmhHaEgzSzNSN2pLeW5oYWdZWVk4MVpVSWlaRnhPWFR5N1VUb2l4ZW9DUTk0UHF3NzlXVms1QVNxWXVlTjFoY0E5QkpMWDN2TGFBVE1kYTlfMWdWTFNmTWYzY1lFc051Zl9VOGVZZmNLNlAtQi0wekkxdllLZ2hna2pwZDJqNV84VWZpeXlnd3N3NUtIcFlMcEFqTWNtOGMtIiwib2F1dGhfdG9rZW4iOiJFQUFCd3pMaXhuallCQU5xenM4VXZoemwzWkFZWkJiTkc1ZnFrMzluRDY3b2tYZUlXY3NaQ29vVDBnMnZ6UG1aQWQzd2tDYXg2ZFlZWkNNUk51R3hsT21NQ3Fsd1dKUGVUNjQzRUZuVjBrN2E4a3RTclBnblpDOWFLeG9tV3djZ1pDZ3VaQmNQZTlTN0psSTg3Q0VuT1pCdmpIVkRpdXBSUU1QTVBqUUNkWkNWNERGSkdLS0hJQkNVNlhLRVhEbzRuWkE2T3BvWkQiLCJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsImlzc3VlZF9hdCI6MTY1MTc1Mzg3OX0; fbsr_124024574287414=zM2yABgZqpJGemiq8pJzm7b9oOH7J-CQIi0mucWPaIE.eyJ1c2VyX2lkIjoiMTAwMDAzMjU2MDc3NDUzIiwiY29kZSI6IkFRRFNlTkhaXzh2MEZUN01CQ040OENqTVdBc1ZpZHBCcm1mMWlDY0JWa0l0NmRkaWpES2dvWGRSMEkyZmtLT200dTlKclhCOExGUDUtSGRvS1pZb2VoMmo5eUZScWlBWjdRTGZhd1NMWFJCRXdacjUzeWFGWjdzTFdsRGIwajZwc1JsTTB3TGYtaXdNaXhGTmMtVHlseEJJZFBUN2FDZUhyMkIyWGcxMzRhR1hOZ2NoS1pmWXR2ZGU2bVZadEZ2MmhHaEgzSzNSN2pLeW5oYWdZWVk4MVpVSWlaRnhPWFR5N1VUb2l4ZW9DUTk0UHF3NzlXVms1QVNxWXVlTjFoY0E5QkpMWDN2TGFBVE1kYTlfMWdWTFNmTWYzY1lFc051Zl9VOGVZZmNLNlAtQi0wekkxdllLZ2hna2pwZDJqNV84VWZpeXlnd3N3NUtIcFlMcEFqTWNtOGMtIiwib2F1dGhfdG9rZW4iOiJFQUFCd3pMaXhuallCQU5xenM4VXZoemwzWkFZWkJiTkc1ZnFrMzluRDY3b2tYZUlXY3NaQ29vVDBnMnZ6UG1aQWQzd2tDYXg2ZFlZWkNNUk51R3hsT21NQ3Fsd1dKUGVUNjQzRUZuVjBrN2E4a3RTclBnblpDOWFLeG9tV3djZ1pDZ3VaQmNQZTlTN0psSTg3Q0VuT1pCdmpIVkRpdXBSUU1QTVBqUUNkWkNWNERGSkdLS0hJQkNVNlhLRVhEbzRuWkE2T3BvWkQiLCJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsImlzc3VlZF9hdCI6MTY1MTc1Mzg3OX0; rur="VLL\0541994601200\0541683290207:01f7d5f494c3681837f53e2ce770da1ddc4052da5591a08544c22d1c800ce817171670b4"'
+        }
         response = requests.get(url ,headers = request_headers)
         cnt = response.json()['data']['media_count']
         return {target : cnt}
     except:
+        print('excepyt')
         response = requests.get(url)
         cnt = response.json()['graphql']['hashtag']['edge_hashtag_to_media']['count']
         return {target : cnt}
--- static/assets/js/main_page.js
+++ static/assets/js/main_page.js
@@ -5,10 +5,14 @@ function hashtag(url) {
 
     // 태그 출력 부모요소
     var box = document.getElementById('tag_output');
+    var legend = document.getElementById('pieID');
 
     try{
         for (var i = 0; i < 20; i++) {
             document.querySelectorAll("#tag_output > span")[0].remove();
+        }
+        for (var i = 0; i < 5; i++) {
+            document.querySelector("#pieID > li")[0].remove();
         }
     } catch (error) {
     } finally {
@@ -26,12 +30,21 @@ function hashtag(url) {
                 alert("fail!");
             },
             success: function (result) {
-                console.log(result['hashtag']);
-
                 for (tag in result['hashtag']) {
                     var tags = document.createElement('span');
                     box.appendChild(tags);
                     tags.innerHTML = result['hashtag'][tag];
+                };
+                for (tag in result['best_hash']) {
+                    var li = document.createElement('li');
+                    var tag_name = document.createElement('em');
+                    var tag_cnt = document.createElement('span');
+                    legend.appendChild(li);
+                    li.appendChild(tag_name);
+                    li.appendChild(tag_cnt);
+                    tag_name.innerHTML = tag;
+                    tag_cnt.innerHTML = result['best_hash'][tag];
+                    createPie(".pieID.legend", ".pieID.pie");
                 };
             }
         });
